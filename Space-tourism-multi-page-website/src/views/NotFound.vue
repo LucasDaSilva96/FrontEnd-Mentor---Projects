86f836d4d1f36246381a1c1d39900d4f8437cd6c
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-09-22 23:51:58 +0200

    Build setting #6

--- Space-tourism-multi-page-website/src/views/NotFound.vue
+++ Space-tourism-multi-page-website/src/views/NotFound.vue
@@ -1,9 +1,12 @@
 <template>
   <section class="w-full h-full flex items-center justify-center">
-    <div class="text-center">
-      <h1 class="text-preset5 font-bold text-white/50">404</h1>
-      <p class="text-white text-preset4">Page not found</p>
+    <div class="flex flex-col items-center justify-center gap-500">
+
+      <div class="text-center">
+        <h1 class="text-preset5 font-bold text-white/50">404</h1>
+        <p class="text-white text-preset4">Page not found</p>
+      </div>
+      <router-link to="/" class="py-300 px-600 bg-white text-darkBlue rounded-md">Home</router-link>
     </div>
-    <router-link to="/" class="py-300 px-600 bg-darkBlue text-white rounded-md">Home</router-link>
   </section>
 </template>
